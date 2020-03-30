ac2e37e2aee02d2d62cc07a41d0b3b7c384fa31d
Maciej Rapacz <maciek.rapacz@gmail.com=>
2020-03-30 21:13:32 +0200

    feat(CLI): Infer main NT name from the input file name

--- ntgen/main.py
+++ ntgen/main.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from typing import Optional
 
 from tap import Tap as TypedArgumentParser
@@ -11,11 +12,11 @@ class ArgumentParser(TypedArgumentParser):
 
     input: str  # Json file containing an object with the data to analyzed
     out: Optional[str] = None  # Destination file to write the Python code to
-    name: str = "NTGenTuple"  # Name of the main NamedTuple
+    name: Optional[str] = None  # Name of the main NamedTuple, if not passed, it will be inferred from the input filename
 
     snake_case: bool = True  # Convert the NamedTuple field names to snake_case
     camel_case: bool = True  # Convert the NamedTuple class names to CamelCase
-    constructors: bool = False  # Insert generic methods that will allow for parsing of the analyzed data structures
+    from_dict: bool = False  # Insert generic methods that will allow for parsing of the analyzed data structures
     as_dict: bool = False  # Insert generic methods allowing for dumping the nested NamedTuple hierarchy to a dict
     max_level: Optional[int] = None  # Specify the max nesting level of the NamedTuple
 
@@ -24,23 +25,26 @@ class ArgumentParser(TypedArgumentParser):
         self.add_argument("input")
         self.add_argument("-s", "--snake-case", action="store_false")
         self.add_argument("-c", "--camel-case", action="store_false")
-        self.add_argument("-f", "--constructors", action="store_true")
-        self.add_argument("-t", "--as-dict", action="store_true")
+        self.add_argument("-f", "--from-dict", action="store_true")
+        self.add_argument("-a", "--as-dict", action="store_true")
 
 
 def main() -> None:
     """Parse arguments and execute ntgen when rune form the command line interface."""
     args = ArgumentParser().parse_args()
 
-    with open(args.input) as f:
+    input_file = Path(args.input)
+    with input_file.open() as f:
         data = json.load(f)
+
+    name = args.name or input_file.stem
 
     rendered_nt = generate_from_dict(
         data=data,
-        name=args.name,
+        name=name,
         snake_case=args.snake_case,
         camel_case=args.camel_case,
-        insert_from_dict=args.constructors,
+        insert_from_dict=args.from_dict,
         insert_as_dict=args.as_dict,
         max_level=args.max_level,
     )
