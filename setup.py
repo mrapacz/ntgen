6bcff1100db72338880e8c3e12d5166fd10d6f5a
Maciej Rapacz <maciek.rapacz@gmail.com=>
2020-04-04 14:21:00 +0200

    fix: Add missing Python 3.6 support tag in setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development",
